4a50eae47b404c5fadb7920c750410041242609b
Rohan <mbakibillahrohan@gmail.com>
2024-03-03 00:08:50 +0600

    refining

--- asset/js/script.js
+++ asset/js/script.js
@@ -5,19 +5,33 @@ let ellipsImg = document.getElementById("ellipsImg");
 let pollyImg = document.getElementById("pollyImg");
 let video = document.querySelector("video");
 
-ellipsImg.addEventListener("click", function (e) {
-  video.play();
-  video.controls = !video.controls;
-  ellipsImg.classList.add("playHidden");
-  pollyImg.classList.add("playHidden");
-});
+// ellipsImg.addEventListener("click", function (e) {
+//   video.play();
+//   video.controls = !video.controls;
+//   ellipsImg.classList.add("playHidden");
+//   pollyImg.classList.add("playHidden");
+// });
 
 // video section js
 
-// stretch arrow js section
-const stretchArrow = document.querySelectorAll(".strech-arrow");
-stretchArrow.forEach((item) => {
-  item.setAttribute("width", 54);
-  // item.setAttribute("height", 20)
-});
-// stretch arrow js section
+
+/** Menu hover effect */
+
+document.getElementById("services_menu").addEventListener("mouseover", ()=>{
+  document.getElementById("dropdown").style.transform = "translateY(0.4vw)";
+})
+
+// document.getElementById("services_menu").addEventListener("mouseleave", ()=>{
+//   setTimeout(()=>{
+//     document.getElementById("dropdown").style.display = "none";
+//   }, 500)
+// })
+
+document.getElementById("dropdown").addEventListener("mouseover", ()=>{
+  
+})
+document.getElementById("dropdown").addEventListener("mouseleave", ()=>{
+  document.getElementById("dropdown").style.transform = "translateY(-17.175vw)";
+})
+
+/** Menu hover effect */
