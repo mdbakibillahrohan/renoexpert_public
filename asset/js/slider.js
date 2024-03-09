61c1848fde409227fd3958251e0b341b75e7bc58
rohan <mbakibillahrohan@gmail.com>
2024-03-09 16:35:35 +0600

    auto sliding done in review section

--- asset/js/slider.js
+++ asset/js/slider.js
@@ -1,6 +1,6 @@
 
 // here started the review section slider 
-new Glider(document.querySelector('.review-slider'), {
+const reviewSlider = new Glider(document.querySelector('.review-slider'), {
     slidesToShow: 1,
     slidesToScroll: 1,
     draggable: true,
@@ -9,20 +9,41 @@ new Glider(document.querySelector('.review-slider'), {
       prev: '.review-prev',
       next: '.review-next'
     },
+    scrollBy: 1,
+    rewind: true,
     responsive: [
       {
         breakpoint: 768,
         settings: {
           // Set to `auto` and provide item width to adjust to viewport
           slidesToShow: 3.2,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           itemWidth: "auto",
         }
       }
     ]
   });
+  let reviewSliderInterval;
 
-//   here ended the review section slider 
+  // Function to start auto sliding
+  function startAutoSlide() {
+    reviewSliderInterval = setInterval(function() {
+      reviewSlider.scrollItem('right');
+    }, 3000); // Change this value to adjust the delay between slides
+  }
+
+  // Function to stop auto sliding
+  function stopAutoSlide() {
+    clearInterval(reviewSliderInterval);
+  }
+
+  // Start auto sliding
+  startAutoSlide();
+
+  // Event listeners to stop and start auto sliding on interaction
+  var gliderContainer = document.querySelector('.review-slider');
+  gliderContainer.addEventListener('mouseover', stopAutoSlide);
+  gliderContainer.addEventListener('mouseout', startAutoSlide);
 
 // here started the services slider 
 new Glider(document.querySelector('.testimonial-slider'), {
