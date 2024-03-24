9677bcf05d8ee2587dff326f63783be6418086a6
devmarufurrahman <mdmarufur5682@gmail.com>
2024-03-25 00:48:32 +0600

    all section done

--- asset/js/script.js
+++ asset/js/script.js
@@ -5,55 +5,62 @@ let ellipsImg = document.getElementById("ellipsImg");
 let pollyImg = document.getElementById("pollyImg");
 let video = document.querySelector("video");
 
-
-// mobile navbar js started here 
+// mobile navbar js started here
 
 const mobileNavbarToggleBtn = document.querySelector("#mobile-nav-toggle-btn");
 const mobileNavbar = document.querySelector("#mobile-navbar-content");
-mobileNavbarToggleBtn.addEventListener("click",  ()=>{
-  if(mobileNavbar.classList.contains("opacity-0")){
+mobileNavbarToggleBtn.addEventListener("click", () => {
+  if (mobileNavbar.classList.contains("opacity-0")) {
     mobileNavbar.classList.remove("opacity-0");
-  }else{
+  } else {
     mobileNavbar.classList.add("opacity-0");
   }
-})
+});
 
+window.addEventListener("scroll", () => {
+  let header = document.querySelector(".header_wrapper");
+  if (document.documentElement.scrollTop > 500) {
+    header.classList.remove("bg-[#EEECE8]");
+    header.classList.add("bg-white");
+  } else {
+    header.classList.remove("bg-white");
+    header.classList.add("bg-[#EEECE8]");
+  }
+});
 
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const menuTriggers = document.querySelectorAll(".menu-trigger");
 
-  menuTriggers.forEach(menuTrigger => {
-      const menuContent = menuTrigger.nextElementSibling;
-      const openIcon = menuTrigger.querySelector(".open-icon");
-      const closedIcon = menuTrigger.querySelector(".closed-icon");
+  menuTriggers.forEach((menuTrigger) => {
+    const menuContent = menuTrigger.nextElementSibling;
+    const openIcon = menuTrigger.querySelector(".open-icon");
+    const closedIcon = menuTrigger.querySelector(".closed-icon");
 
-      menuTrigger.addEventListener("click", function() {
-          const isOpen = menuContent.style.display === "block";
-          if (isOpen) {
-              menuContent.style.display = "none";
-              openIcon.style.display = "none";
-              closedIcon.style.display = "block";
-          } else {
-              menuContent.style.display = "block";
-              openIcon.style.display = "block";
-              closedIcon.style.display = "none";
-          }
-      });
+    menuTrigger.addEventListener("click", function () {
+      const isOpen = menuContent.style.display === "block";
+      if (isOpen) {
+        menuContent.style.display = "none";
+        openIcon.style.display = "none";
+        closedIcon.style.display = "block";
+      } else {
+        menuContent.style.display = "block";
+        openIcon.style.display = "block";
+        closedIcon.style.display = "none";
+      }
+    });
 
-      // Close menu when clicking outside of it
-      document.addEventListener("click", function(event) {
-          if (!menuTrigger.contains(event.target) && !menuContent.contains(event.target)) {
-              menuContent.style.display = "none";
-              openIcon.style.display = "none";
-              closedIcon.style.display = "block";
-          }
-      });
+    // Close menu when clicking outside of it
+    document.addEventListener("click", function (event) {
+      if (
+        !menuTrigger.contains(event.target) &&
+        !menuContent.contains(event.target)
+      ) {
+        menuContent.style.display = "none";
+        openIcon.style.display = "none";
+        closedIcon.style.display = "block";
+      }
+    });
   });
 });
 
-
-
-// mobile navbar js ended here 
+// mobile navbar js ended here
--- asset/js/slider.js
+++ asset/js/slider.js
@@ -1,59 +1,97 @@
-
-// here started the review section slider 
-const reviewSlider = new Glider(document.querySelector('.review-slider'), {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    draggable: true,
-    dots: '.review-dots',
-    arrows: {
-      prev: '.review-prev',
-      next: '.review-next'
+// here started the review section slider
+const reviewSlider = new Glider(document.querySelector(".review-slider"), {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: true,
+  dots: ".review-dots",
+  arrows: {
+    prev: ".review-prev",
+    next: ".review-next",
+  },
+  scrollBy: 1,
+  rewind: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 3.2,
+        slidesToScroll: 1,
+        itemWidth: "auto",
+      },
     },
-    scrollBy: 1,
-    rewind: true,
+  ],
+});
+let reviewSliderInterval;
+
+// Function to start auto sliding
+function startAutoSlide() {
+  reviewSliderInterval = setInterval(function () {
+    reviewSlider.scrollItem("right");
+  }, 3000); // Change this value to adjust the delay between slides
+}
+
+// Function to stop auto sliding
+function stopAutoSlide() {
+  clearInterval(reviewSliderInterval);
+}
+
+// Start auto sliding
+startAutoSlide();
+
+// Event listeners to stop and start auto sliding on interaction
+var gliderContainer = document.querySelector(".review-slider");
+gliderContainer.addEventListener("mouseover", stopAutoSlide);
+gliderContainer.addEventListener("mouseout", startAutoSlide);
+
+// here started the services slider
+const testimonialSlider = new Glider(
+  document.querySelector(".testimonial-slider"),
+  {
+    slidesToShow: 1,
+    draggable: true,
+    arrows: {
+      prev: ".testimonial-prev-m",
+      next: ".testimonial-next-m",
+    },
     responsive: [
       {
         breakpoint: 768,
         settings: {
           // Set to `auto` and provide item width to adjust to viewport
-          slidesToShow: 3.2,
+          slidesToShow: 1.2,
           slidesToScroll: 1,
           itemWidth: "auto",
-        }
-      }
-    ]
-  });
-  let reviewSliderInterval;
-
-  // Function to start auto sliding
-  function startAutoSlide() {
-    reviewSliderInterval = setInterval(function() {
-      reviewSlider.scrollItem('right');
-    }, 3000); // Change this value to adjust the delay between slides
+          itemHeight: "200",
+          arrows: {
+            prev: ".testimonial-prev",
+            next: ".testimonial-next",
+          },
+        },
+      },
+    ],
   }
+);
 
-  // Function to stop auto sliding
-  function stopAutoSlide() {
-    clearInterval(reviewSliderInterval);
-  }
+let testimonialSliderInterval;
 
-  // Start auto sliding
-  startAutoSlide();
+// Function to start auto sliding
+function startAutoSlideTestimonial() {
+  testimonialSliderInterval = setInterval(function () {
+    testimonialSlider.scrollItem("right");
+  }, 3000); // Change this value to adjust the delay between slides
+}
 
-  // Event listeners to stop and start auto sliding on interaction
-  var gliderContainer = document.querySelector('.review-slider');
-  gliderContainer.addEventListener('mouseover', stopAutoSlide);
-  gliderContainer.addEventListener('mouseout', startAutoSlide);
+// Function to stop auto sliding
+function stopAutoSlideTestimonial() {
+  clearInterval(testimonialSliderInterval);
+}
 
-// here started the services slider 
-new Glider(document.querySelector('.testimonial-slider'), {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    draggable: true,
-    dots: '.dots',
-    arrows: {
-      prev: '.testimonial-prev',
-      next: '.testimonial-next'
-    }
-  });
-// here ended the services slider 
+// Start auto sliding
+startAutoSlideTestimonial();
+
+// Event listeners to stop and start auto sliding on interaction
+var testimonialContainer = document.querySelector(".testimonial-slider");
+testimonialContainer.addEventListener("mouseover", stopAutoSlideTestimonial);
+testimonialContainer.addEventListener("mouseout", startAutoSlideTestimonial);
+// here ended the services slider
